32c45b1b0fba142d4dcb2c6ec0e7289c43222755
jun <wnsgus3132@naver.com>
2024-07-30 22:18:21 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/jpabook/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpabook.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpabook.domain.Category;
+import jpabook.jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직 == (엔티티안에 하면 좀더 객체지향적임)//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
