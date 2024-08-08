d5ac6c81291dde6d869d7777d109fe153ae2866d
junyongg <wnsgus3132@naver.com>
2024-08-08 17:44:20 +0900

    회원 조회, 상품등록 추가

--- src/main/java/jpabook/jpabook/controller/MemberController.java
+++ src/main/java/jpabook/jpabook/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -40,8 +42,18 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
-
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        //화면에 필요한 데이터를 찍어서 보여주는 템플릿 엔진(지금 한서버에서 타임리프 사용중)에서는 엔티티를 사용해도 괜찮지만 api를 만들때는 절대 엔티티를 그대로 뿌리면
+        //안되고 Form이나 DTO를 만들어서 뿌리는것이 좋다.
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
+
 
 
 }
